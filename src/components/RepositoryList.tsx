7e4219529009262d7df085996b1639fd4818d9cd
Roger Oliveira <rogerpolvr@gmail.com>
2021-04-09 01:52:48 -0300

    feat(extra): Implementando o input de username

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 
-const username = "rogerpolvr";
-
 interface Repository {
   id: number;
   name: string;
@@ -13,25 +11,39 @@ interface Repository {
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [username, setUsername] = useState<string>("");
+
+  let inputData: string = "";
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/${username}/repos`)
-      .then((response) => response.json())
-      .then((data) => setRepositories(data));
-  }, []);
+    if (username) {
+      fetch(`https://api.github.com/users/${username}/repos`)
+        .then((response) => response.json())
+        .then((data) => setRepositories(data));
+    }
+  }, [username]);
+
+  function getInputData(data: { target: { value: string } }) {
+    inputData = data.target.value;
+  }
 
   return (
     <div className="repository-container">
       <section className="repository-list">
         <h1>Github repository list</h1>
         <div className="repository-input">
-          <input placeholder="Enter the github username" />
-          <button>Load</button>
+          <input
+            placeholder="Enter the github username"
+            onChange={getInputData}
+          />
+          <button onClick={() => setUsername(inputData)}>Load</button>
         </div>
         <ul>
-          {repositories.map((repository) => (
-            <RepositoryItem key={repository.id} repository={repository} />
-          ))}
+          {username
+            ? repositories.map((repository) => (
+                <RepositoryItem key={repository.id} repository={repository} />
+              ))
+            : null}
         </ul>
       </section>
     </div>
